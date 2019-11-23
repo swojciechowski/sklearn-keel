6358887c3be7dfd0a87d28a2b64fa310317ea543
Szymon Wojciechowski <bluetapeon@gmail.com>
2019-11-23 15:19:31 +0100

    organize

--- src/keel/keel.py
+++ src/keel/keel.py
@@ -8,10 +8,8 @@ import pandas as pd
 from sklearn.utils import Bunch
 from sklearn.preprocessing import LabelEncoder
 
-from typing import NamedTuple
-
 STORAGE_DIR = os.path.join(os.path.dirname(__file__), ".storage")
-
+#TODO: Add fetching from KEEL, until then needs to be downloaded manually.
 
 def find_datasets():
     for dirpath, _, filenames in os.walk(STORAGE_DIR):
@@ -20,16 +18,13 @@ def find_datasets():
             yield os.path.relpath(dirpath, STORAGE_DIR)
 
 
-# TODO: Add downloading datasets
-KEEL_DATASETS = tuple(find_datasets())
-
-
 def parse_keel_dat(dat_file):
     with open(dat_file, "r") as fp:
         data = fp.read()
         header, payload = data.split("@data\n")
 
-    attributes = re.findall(r"@[Aa]ttribute (.*?)[ {](integer|real|.*)", header)
+    attributes = re.findall(
+        r"@[Aa]ttribute (.*?)[ {](integer|real|.*)", header)
     output = re.findall(r"@[Oo]utput[s]? (.*)", header)
 
     dtype_map = {"integer": np.int, "real": np.float}
@@ -45,32 +40,19 @@ def parse_keel_dat(dat_file):
     return data, target
 
 
-def load_dataset(dataset_name, return_X_y=False):
-    if dataset_name not in KEEL_DATASETS:
-        raise ValueError(f"Can't find dataste {dataset_name}.")
-
-    data_file = os.path.join(STORAGE_DIR, dataset_name, f"{os.path.basename(dataset_name)}.dat")
-    data, target = parse_keel_dat(data_file)
-
+def prepare_X_y(data, target):
     class_encoder = LabelEncoder()
     target = class_encoder.fit_transform(target.values.ravel())
-    target_names = class_encoder.classes_
+    return data.values, target
+
+
+def load_dataset(dataset_name, return_X_y=False):
+    data_file = os.path.join(STORAGE_DIR, dataset_name, f"{os.path.basename(dataset_name)}.dat")
+    # TODO: Add checking if set exists
+
+    data, target = parse_keel_dat(data_file)
 
     if return_X_y:
-        return data.values, target
-    else:
-        return Bunch(
-            **{
-                "data": data,
-                "target": target,
-                "target_names": target_names,
-                "filename": data_file,
-            }
-        )
+        return prepare_X_y(data, target)
 
-
-if __name__ == "__main__":
-    for _ in KEEL_DATASETS:
-        data = load_dataset(_)
-        print(_)
-        # print(data)
+    return Bunch(data=data, target=target, filename=data_file)
